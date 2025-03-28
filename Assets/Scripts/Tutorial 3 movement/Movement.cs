01f6825330fcb6b3c010a330018a4ab1d0058de3
Sammy <frnkverdam@gmail.com>
2025-03-28 12:13:30 +0100

    Springen op Platforms

---
+++ Assets/Scripts/Tutorial 3 movement/Movement.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+        Vector3 move = new Vector3(moveX, 0, moveZ) * speed * Time.deltaTime;
+        rb.MovePosition(transform.position + move);
+    }
+}
