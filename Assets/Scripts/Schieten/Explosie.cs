9766c2b9f932b60f0b3937ea339d16840e143df8
Sammy <frnkverdam@gmail.com>
2025-04-10 12:21:58 +0200

    Schieten en obstakels

---
+++ Assets/Scripts/Schieten/Explosie.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Explosion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private GameObject particle;
+    [SerializeField] private GameObject explosion;
+    [SerializeField] private string tag;
+    public GameObject player;
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.collider.tag == tag)
+        {
+            explosion = Instantiate(particle, transform.position, transform.rotation);
+            Destroy(explosion, 2f);
+            Destroy(this.transform.gameObject);
+
+            if (tag == "Enemy")
+            {
+                Destroy(this.transform.gameObject);
+            }
+        }
+
+    }
+}
